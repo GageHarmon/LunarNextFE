fb7ac0b309cc07a1d40e6f7ddd0ea2c8c17d8c51
Gage Harmon <gaharmon1@gmail.com>
2023-05-04 17:04:26 -0600

    commented out chatgpt stuff to avoid deploy errors

--- lunarnext/src/pages/api/openai/route.ts
+++ lunarnext/src/pages/api/openai/route.ts
@@ -1,50 +1,50 @@
-import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
-import { NextResponse } from "next/server";
-import { AxiosResponse } from "axios";
+// import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
+// import { NextResponse } from "next/server";
+// import { AxiosResponse } from "axios";
 
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-export async function POST(request: Request, response: any) {
-    try {
-      const { title, role } = await request.json();
-  
-      const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> =
-        await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [
-            {
-              role: "user",
-              // content: `Create small blog post with html tags based on this title: ${title}`,
-              content: `Help me solve my IT problems`,
-            },
-            {
-              role: "system",
-              content: `${
-                role || "I am a helpful assistant"
-              }. I can help you with your IT problems. `,
-            },
-          ],
-        });
-  
-      // response.revalidate("/api/posts")
-      return NextResponse.json(
-        {
-          content: aiResponse.data.choices[0].message?.content,
-        },
-        { status: 200 }
-      );
-    } catch (error) {
-      console.error("request error", error);
-      NextResponse.json({ error: "error updating post" }, { status: 500 });
-    }
-  }
-
-// const completion = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "Hello world",
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
 // });
-// console.log(completion.data.choices[0].text);
+// const openai = new OpenAIApi(configuration);
+
+// export async function POST(request: Request, response: any) {
+//     try {
+//       const { title, role } = await request.json();
+  
+//       const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> =
+//         await openai.createChatCompletion({
+//           model: "gpt-3.5-turbo",
+//           messages: [
+//             {
+//               role: "user",
+//               // content: `Create small blog post with html tags based on this title: ${title}`,
+//               content: `Help me solve my IT problems`,
+//             },
+//             {
+//               role: "system",
+//               content: `${
+//                 role || "I am a helpful assistant"
+//               }. I can help you with your IT problems. `,
+//             },
+//           ],
+//         });
+  
+//       // response.revalidate("/api/posts")
+//       return NextResponse.json(
+//         {
+//           content: aiResponse.data.choices[0].message?.content,
+//         },
+//         { status: 200 }
+//       );
+//     } catch (error) {
+//       console.error("request error", error);
+//       NextResponse.json({ error: "error updating post" }, { status: 500 });
+//     }
+//   }
+
+// // const completion = await openai.createCompletion({
+// //   model: "text-davinci-003",
+// //   prompt: "Hello world",
+// // });
+// // console.log(completion.data.choices[0].text);
